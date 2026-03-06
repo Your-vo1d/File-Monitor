6432df87a936acdaea3abf8a190214e54d40fc29
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-06 13:59:22 +0700

    Перемещение main и добавление интерфейса для логирования

---
+++ include/Ilog.h
@@ -0,0 +1,82 @@
+﻿#ifndef ILOG_H
+#define ILOG_H
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <QTime>
+#include <QObject>
+
+//Определяет методы для обработки событий файловой системы.
+class ILog : public QObject
+{
+    Q_OBJECT
+
+public:
+    virtual ~ILog() = default;
+
+    //Записывает сообщение в лог.
+
+    virtual void log(const std::string &data) = 0;
+
+public slots:
+
+    //Срабатывает при обнаружении файла.
+    virtual void onFileExistence(IFileContainer *container, int index) = 0;
+
+    //Срабатывает при обновлении файла.
+    virtual void onFileUpdate(IFileContainer *container, int index) = 0;
+
+    //Срабатывает при удалении файла.
+    virtual void onFileRemoval(IFileContainer *container, int index) = 0;
+
+    //Срабатывает в конце цикла обработки файлов
+    virtual void onCycleEnd() = 0;
+};
+
+
+class ConsoleLog : public ILog
+{
+    Q_OBJECT
+
+private:
+
+    //Текущее время (используется для вывода временной метки)
+    QTime m_time;
+
+    //Флаг вывода времени перед сообщением
+    bool m_logTime = false;
+
+    //Преобразует qint64 в std::string
+    static std::string qint64ToString(qint64 value);
+
+public:
+
+    ~ConsoleLog() = default;
+
+    //Конструктор
+    explicit ConsoleLog(bool logTime);
+
+    //Вывод сообщения в консоль
+    void log(const std::string &data) override;
+
+    //Очистка консоли
+    void clearConsole();
+
+public slots:
+
+    //Обработка события обнаружения файла
+    void onFileExistence(IFileContainer *container, int index) override;
+
+    //Обработка события обновления файла
+    void onFileUpdate(IFileContainer *container, int index) override;
+
+    //Обработка события удаления файла
+    void onFileRemoval(IFileContainer *container, int index) override;
+
+    //Завершение цикла обработки
+    void onCycleEnd() override;
+};
+
+#endif // ILOG_H
---
+++ src/ConsoleLog.cpp
@@ -0,0 +1,81 @@
+﻿#include "ILog.h"
+
+#include <QFileInfo>
+#include <algorithm>
+#include <cstdlib>
+
+ConsoleLog::ConsoleLog(bool logTime)
+{
+    // Устанавливает, нужно ли выводить временные метки в логах
+    m_logTime = logTime;
+}
+
+//Очищение консоли
+void ConsoleLog::clearConsole()
+{
+    system("cls");
+}
+
+//Вывод сообщения в консоль
+void ConsoleLog::log(const std::string &data)
+{
+    if (m_logTime) {
+        std::cout << m_time.currentTime().toString().toStdString() << ' ';
+    }
+
+    std::cout << "[LOG] " << data << '\n';
+}
+
+std::string ConsoleLog::qint64ToString(qint64 value)
+{
+    std::string result;
+    result.reserve(20); // максимум 20 цифр для qint64
+
+    qint64 number = value;
+    do {
+        result += "0123456789"[number % 10];
+        number /= 10;
+    } while (number);
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
+
+void ConsoleLog::onFileExistence(IFileContainer *container, int index)
+{
+    // Случай: файл существует и не был изменён
+    QFileInfo file = (*container)[index];
+
+    log(std::to_string(index) + ": "
+        + file.absoluteFilePath().toStdString()
+        + " | "
+        + qint64ToString(file.size()));
+}
+
+void ConsoleLog::onFileUpdate(IFileContainer *container, int index)
+{
+    // Случай: файл существует и был обновлён
+    QFileInfo file = (*container)[index];
+
+    log(std::to_string(index) + ": [UPDATED "
+        + file.lastModified().time().toString().toStdString()
+        + "] "
+        + file.absoluteFilePath().toStdString()
+        + " | "
+        + qint64ToString(file.size()));
+}
+
+void ConsoleLog::onFileRemoval(IFileContainer *container, int index)
+{
+    // Случай: файл был удалён или не существует
+    QFileInfo file = (*container)[index];
+
+    log(std::to_string(index) + ": [NOT EXISTS] "
+        + file.absoluteFilePath().toStdString());
+}
+
+void ConsoleLog::onCycleEnd()
+{
+    // Завершение цикла: очистка консоли
+    clearConsole();
+}
