f82eed001127ade5bdb2b20a8779ff3e4795d8fd
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-13 13:17:55 +0700

    Добавлен интерфейс для хранения информации о файлах

---
+++ include/IFileContainer.h
@@ -0,0 +1,18 @@
+﻿#ifndef IFILECONTAINER_H
+#define IFILECONTAINER_H
+
+#include <QObject>
+#include <QFileInfo>
+
+class IFileContainer
+{
+public:
+    virtual ~IFileContainer() = default;
+    virtual QFileInfo operator[](int index) = 0;
+    virtual void append(QFileInfo file) = 0;
+    virtual void clear() = 0;
+    virtual int length() = 0;
+};
+
+#endif // IFILECONTAINER_H
+
