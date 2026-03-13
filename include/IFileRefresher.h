f73ea64f208d8e497e34abc478871dd1e48d3b4c
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-13 13:18:41 +0700

    Добавлен интерфейс для обновления информации о файлах

---
+++ include/IFileRefresher.h
@@ -0,0 +1,11 @@
+﻿#ifndef IFILEREFRESHER_H
+#define IFILEREFRESHER_H
+
+class IFileRefresher
+{
+public:
+    virtual ~IFileRefresher() = default;
+    virtual void refresh() = 0;
+};
+
+#endif // IFILEREFRESHER_H
