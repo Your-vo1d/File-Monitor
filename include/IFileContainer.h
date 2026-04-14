5993526fa48c818b66d5ef40eff488d5897f1f4a
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-04-15 02:20:42 +0700

    feat: change paramether for append for IFileContainer and add remove method

--- include/IFileContainer.h
+++ include/IFileContainer.h
@@ -7,8 +7,9 @@ class IFileContainer
 public:
     virtual ~IFileContainer() = default;
     virtual QFileInfo operator[](int index) = 0;
-    virtual void append(QFileInfo file) = 0;
+    virtual bool append(const QString& path) = 0;
+    virtual bool remove(const QString& path) = 0;
     virtual void clear() = 0;
-    virtual int length() = 0;
+    virtual int length() const = 0;
 };
 #endif // IFILECONTAINER_H
