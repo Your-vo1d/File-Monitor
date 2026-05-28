be8fb506bcf0550882a163655ff2eba77ac6ed58
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-05-28 13:37:49 +0700

    feat: fix logger output

--- include/ConsoleLog.h
+++ include/ConsoleLog.h
@@ -1,27 +1,23 @@
-﻿#ifndef CONSOLELOG_H
+#ifndef CONSOLELOG_H
 #define CONSOLELOG_H
 
 #include "ILog.h"
-#include <QTime>
-#include <string>
+#include <QTextStream>
+#include <QDateTime>
 
 class ConsoleLog : public ILog
 {
     Q_OBJECT
-private:
-    bool m_logTime;
-    static std::string qint64ToString(qint64 value);
-
+    QTextStream m_stdout;
+    bool m_showTimestamp;
 public:
-    explicit ConsoleLog(bool logTime = false);
-    ~ConsoleLog() override = default;
-
-    void log(const std::string &data) override;
+    explicit ConsoleLog(bool showTimestamp = false, QObject* parent = nullptr);
+    void log(const QString& message) override;
 
 public slots:
-    void onFileExistence(IFileContainer *container, int index) override;
-    void onFileUpdate(IFileContainer *container, int index) override;
-    void onFileRemoval(IFileContainer *container, int index) override;
+    void onFileExistence(IFileContainer* container, int index) override;
+    void onFileUpdate(IFileContainer* container, int index) override;
+    void onFileRemoval(IFileContainer* container, int index) override;
 };
 
 #endif // CONSOLELOG_H
